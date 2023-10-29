6b3b7290bdfbe8bd69eb5e432f5f450515dbc052
Eduardo Horta <eduhorta89@gmail.com>
2023-10-29 15:31:21 -0300

    Setting up React Query and Toast

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,11 +1,28 @@
 'use client';
 import { uploadToS3 } from '@/lib/s3';
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
 import { Inbox } from 'lucide-react';
 import React from 'react';
 import { useDropzone } from 'react-dropzone';
+import toast from 'react-hot-toast';
 
-type Props = {};
 const FileUpload = () => {
+  const { mutate } = useMutation({
+    mutationFn: async ({
+      file_key,
+      file_name,
+    }: {
+      file_key: string;
+      file_name: string;
+    }) => {
+      const response = await axios.post('/api/create-chat', {
+        file_key,
+        file_name,
+      });
+      return response.data;
+    },
+  });
   const { getRootProps, getInputProps } = useDropzone({
     accept: { 'application/pdf': ['.pdf'] },
     maxFiles: 1,
@@ -14,12 +31,25 @@ const FileUpload = () => {
       const file = acceptedFiles[0];
       if (file.size > 10 * 1024 * 1024) {
         // greater than 10 mb
+        toast.error('File too large');
         alert('please upload a smaller file');
         return;
       }
 
       try {
         const data = await uploadToS3(file);
+        if (!data?.file_key || !data.file_name) {
+          alert('something went wrong');
+          return;
+        }
+        mutate(data, {
+          onSuccess: (data) => {
+            console.log(data);
+          },
+          onError: (err) => {
+            console.log(err);
+          },
+        });
         console.log('data', data);
       } catch (error) {
         console.log(error);
