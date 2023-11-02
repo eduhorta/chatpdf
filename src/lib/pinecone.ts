b59ff61e5b6fd9bdd939c6ce39c627536b834218
Eduardo Horta <eduhorta89@gmail.com>
2023-11-02 14:35:54 -0300

    Still setting up PineconeDB

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,12 +1,15 @@
-import { Pinecone, PineconeClient } from '@pinecone-database/pinecone'
+import { Pinecone, PineconeClient, Vector, utils as PineconeUtils } from '@pinecone-database/pinecone'
 import { downloadFromS3 } from './s3-server'
 import {PDFLoader} from 'langchain/document_loaders/fs/pdf'
 import {Document, RecursiveCharacterTextSplitter} from '@pinecone-database/doc-splitter'
+import { getEmbedding } from './embeddings'
+import md5 from 'md5'
+import { convertToAscii } from './utils'
 
 
 let pinecone: PineconeClient | null = null
 
-export const getPinecone = async () => {
+export const getPineconeClient = async () => {
     if(!pinecone) {
         pinecone = new PineconeClient()
         await pinecone.init({
@@ -33,11 +36,61 @@ export async function loadS3IntoPinecone(fileKey: string) {
     }
     const loader = new PDFLoader(file_name)
     const pages = (await loader.load()) as PDFPage[]
+
     // 2. Split and segment the PDF into smaller documents
-    return pages
+    const documents = await Promise.all(pages.map(prepareDocument))
+
+    // 3. Vectorise and embed individual documents
+    const vectors = await Promise.all(documents.flat().map(embedDocument))
+
+    // 4. Upload to PineconeDB
+    const client = await getPineconeClient()
+    const pineconeIndex = client.Index('chatpdf')
+
+    console.log('Inserting Vectors into pinecone')
+    const namespace = convertToAscii(fileKey)
+    PineconeUtils.chunkedUpsert(pineconeIndex, vectors, namespace, 10)
+
+    return documents[0]
+}
+
+async function embedDocument(doc: Document) {
+    try {
+        const embeddings = await getEmbedding(doc.pageContent)
+        const hash = md5(doc.pageContent)
+
+        return {
+            id: hash,
+            values: embeddings,
+            metadata: {
+                text: doc.metadata.text,
+                pageNumber: doc.metadata.pageNumber
+            }
+        } as Vector
+    } catch (error) {
+        console.log('error embedding document', error)
+        throw error
+    }
+}
+
+export const truncateStringByBytes = (str: string, bytes: number) => {
+    const enc = new TextEncoder()
+    return new TextDecoder('utf-8').decode(enc.encode(str).slice(0, bytes))
 }
 
 async function prepareDocument(page: PDFPage) {
     let {pageContent, metadata} = page
     pageContent = pageContent.replace(/\n/g, '')
+    // Split the Doc
+    const splitter = new RecursiveCharacterTextSplitter()
+    const docs = await splitter.splitDocuments([
+        new Document({
+            pageContent,
+            metadata: {
+                pageNumber: metadata.loc.pageNumber,
+                text: truncateStringByBytes(pageContent, 36000)
+            }
+        })
+    ])
+    return docs
 }
